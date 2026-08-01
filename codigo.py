63ba5af0f8b8332cc9f5c92bf9787d9c0406af26
Jesus gomez <230648411+jesusgomez2004@users.noreply.github.com>
2026-08-01 10:50:06 -0300

    subiendo el archivo .py

---
+++ codigo.py
@@ -0,0 +1,13 @@
+nombre = 'jesus' 
+apellido = 'gomez'
+
+saludo = 'hola '+nombre+' '+apellido + " como estas ?" 
+print(saludo)
+
+
+mensaje = saludo + """
+Esta cordialmente invitado a la boda el presidente de albana califormia en nueva zelanda
+Atte
+Dr. jhon J. lewis"""
+
+print(mensaje)
